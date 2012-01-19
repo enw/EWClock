941e11fe44f302b81fc56bade50f934bd903e09f
Elliot Winard <elliot.winard@frogdesign.com>
2012-01-19 18:32:44 -0500

    add bigclock

--- js/script.js
+++ js/script.js
@@ -22,27 +22,11 @@ $(function () {
     $("#second"+now.getSeconds()).addClass("current");
     $("#minute"+now.getMinutes()).addClass("current");
     $("#hour"+now.getHours()).addClass("current");
+
+    $("#bigtime").removeClass("hidden");
+    $("#hour").text(now.getHours());
+    $("#minute").text(now.getMinutes());
+    $("#second").text(now.getSeconds());
   }
   var iid = setInterval( tick, MS_BETWEEN_TICKS );
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
